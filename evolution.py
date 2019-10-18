d8e4cfb0dd6e163467a39df1f481ff00b588d85c
karinemiras <karine.smiras@gmail.com>
2019-10-18 17:10:19 +0200

    try catch lvie

--- evolution.py
+++ evolution.py
@@ -5,29 +5,35 @@ import time
 
 def go_live_ableton(song):
 
-    os.system('all_params='
-              + str(song.instruments)
-              + ' osascript as_open.scpt')
-    time.sleep(1)
+    try:
+        os.system('all_params='
+                  + str(song.instruments)
+                  + ' osascript as_open.scpt')
+        time.sleep(1)
 
-    set = live.Set()
-    set.scan(scan_devices=True)
-    set.tempo = song.tempo
+        set = live.Set()
+        set.scan(scan_devices=True)
+        set.tempo = song.tempo
 
-    # play all tracks
-    for t in set.tracks:
-        t.clips[0].play()
+        # play all tracks
+        for t in set.tracks:
+            t.clips[0].play()
 
-    # wait for song to finish playing
-    time.sleep(song.get_song_duration())
+        # wait for song to finish playing
+        time.sleep(song.get_song_duration())
 
-    # stop all tracks
-    for t in set.tracks:
-        t.clips[0].stop()
+        # stop all tracks
+        for t in set.tracks:
+            t.clips[0].stop()
 
-    # saves project and closes ableton
-    os.system('osascript as_close.scpt')
-    time.sleep(1)
+        # saves project and closes ableton
+        os.system('osascript as_close.scpt')
+        time.sleep(1)
+
+        return True
+
+    except:
+        return False
 
 
 for i in range(1, 43):
@@ -38,5 +44,9 @@ for i in range(1, 43):
     song.export_midi(song_name)
     song.export_midi('current_song_all')
 
-    go_live_ableton(song)
+    went_live = False
+
+    while not went_live:
+        went_live = go_live_ableton(song)
+
 
